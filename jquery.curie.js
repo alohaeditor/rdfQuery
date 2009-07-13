1bc87398a1f9aaa79d3bb93821dc197cc0c0d143
jeni.tennison <jeni.tennison@70de001c-9c6e-11dd-979b-47109e308e52>
2009-07-13 09:49:13 +0000

    Fix for jquery.curie.js if opts.reservedNamespace is undefined.
    git-svn-id: http://rdfquery.googlecode.com/svn/trunk@164 70de001c-9c6e-11dd-979b-47109e308e52

--- jquery.curie.js
+++ jquery.curie.js
@@ -5,7 +5,7 @@
  * Licensed under the MIT (MIT-LICENSE.txt)
  *
  * Depends:
- *	jquery.uri.js
+ *  jquery.uri.js
  *  jquery.xmlns.js
  */
 
@@ -30,8 +30,8 @@
     * @param {string} [options.defaultNamespace]  The namespace to apply to a CURIE with no prefix which is not mapped to the reserved namespace by the rules given above.
     * @param {Object} [options.namespaces] A map of namespace bindings used to map CURIE prefixes to URIs.
     * @param {string[]} [options.reserved=['alternate', 'appendix', 'bookmark', 'cite', 'chapter', 'contents', 'copyright',
-			'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
-			'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
+      'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
+      'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
                         A list of local names that will always be mapped to the URI specified by reservedNamespace.
     * @param {string} [options.charcase='preserve'] Specifies the case of the returned URI. Acceptable values are:
     * <dl>
@@ -42,50 +42,50 @@
     * If any other value is provided or the option is not specified, a default value of 'preserve' will be assumed.
     * @return A new {@link jQuery.uri} object representing the full absolute URI specified by the CURIE.
     */
-	$.curie = function (curie, options) {
-		var
-			opts = $.extend({}, $.curie.defaults, options || {}),
-			m = /^(([^:]*):)?(.+)$/.exec(curie),
-			prefix = m[2],
-			local = m[3],
-			ns = opts.namespaces[prefix];
-		if (/^:.+/.test(curie)) { // This is the case of a CURIE like ":test"
-		  if (opts.reservedNamespace === undefined || opts.reservedNamespace === null) {
-		    throw "Malformed CURIE: No prefix and no default namespace for unprefixed CURIE " + curie;
-		  } else {
-  		  ns = opts.reservedNamespace;
-		  }
-		} else if (prefix) {
-		  if (ns === undefined) {
-  			throw "Malformed CURIE: No namespace binding for " + prefix + " in CURIE " + curie;
-		  }
-		} else {
-		  if (opts.charcase === 'lower') {
-		    curie = curie.toLowerCase();
-		  } else if (opts.charcase === 'upper') {
-		    curie = curie.toUpperCase();
-		  }
-  		if (opts.reserved.length && $.inArray(curie, opts.reserved) >= 0) {
-  		  ns = opts.reservedNamespace;
-  		  local = curie;
+  $.curie = function (curie, options) {
+    var
+      opts = $.extend({}, $.curie.defaults, options || {}),
+      m = /^(([^:]*):)?(.+)$/.exec(curie),
+      prefix = m[2],
+      local = m[3],
+      ns = opts.namespaces[prefix];
+    if (/^:.+/.test(curie)) { // This is the case of a CURIE like ":test"
+      if (opts.reservedNamespace === undefined || opts.reservedNamespace === null) {
+        throw "Malformed CURIE: No prefix and no default namespace for unprefixed CURIE " + curie;
+      } else {
+        ns = opts.reservedNamespace;
+      }
+    } else if (prefix) {
+      if (ns === undefined) {
+        throw "Malformed CURIE: No namespace binding for " + prefix + " in CURIE " + curie;
+      }
+    } else {
+      if (opts.charcase === 'lower') {
+        curie = curie.toLowerCase();
+      } else if (opts.charcase === 'upper') {
+        curie = curie.toUpperCase();
+      }
+      if (opts.reserved.length && $.inArray(curie, opts.reserved) >= 0) {
+        ns = opts.reservedNamespace;
+        local = curie;
       } else if (opts.defaultNamespace === undefined || opts.defaultNamespace === null) {
-  			// the default namespace is provided by the application; it's not clear whether
-  			// the default XML namespace should be used if there's a colon but no prefix
-  			throw "Malformed CURIE: No prefix and no default namespace for unprefixed CURIE " + curie;
-  		} else {
-  		  ns = opts.defaultNamespace;
-  		}
-		}
-	  return $.uri(ns + local);
-	};
+        // the default namespace is provided by the application; it's not clear whether
+        // the default XML namespace should be used if there's a colon but no prefix
+        throw "Malformed CURIE: No prefix and no default namespace for unprefixed CURIE " + curie;
+      } else {
+        ns = opts.defaultNamespace;
+      }
+    }
+    return $.uri(ns + local);
+  };
 
-	$.curie.defaults = {
-		namespaces: {},
-		reserved: [],
-		reservedNamespace: undefined,
-		defaultNamespace: undefined,
-		charcase: 'preserve'
-	};
+  $.curie.defaults = {
+    namespaces: {},
+    reserved: [],
+    reservedNamespace: undefined,
+    defaultNamespace: undefined,
+    charcase: 'preserve'
+  };
 
    /**
     * Creates a {@link jQuery.uri} object by parsing a safe CURIE string (a CURIE
@@ -99,8 +99,8 @@
     * @param {string} [options.defaultNamespace]  The namespace to apply to a CURIE with no prefix which is not mapped to the reserved namespace by the rules given above.
     * @param {Object} [options.namespaces] A map of namespace bindings used to map CURIE prefixes to URIs.
     * @param {string[]} [options.reserved=['alternate', 'appendix', 'bookmark', 'cite', 'chapter', 'contents', 'copyright',
-			'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
-			'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
+      'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
+      'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
                         A list of local names that will always be mapped to the URI specified by reservedNamespace.
     * @param {string} [options.charcase='preserve'] Specifies the case of the returned URI. Acceptable values are:
     * <dl>
@@ -111,10 +111,10 @@
     * If any other value is provided or the option is not specified, a default value of 'preserve' will be assumed.
     * @return A new {@link jQuery.uri} object representing the full absolute URI specified by the CURIE.
     */
-	$.safeCurie = function (safeCurie, options) {
-		var m = /^\[([^\]]+)\]$/.exec(safeCurie);
-		return m ? $.curie(m[1], options) : $.uri(safeCurie);
-	};
+  $.safeCurie = function (safeCurie, options) {
+    var m = /^\[([^\]]+)\]$/.exec(safeCurie);
+    return m ? $.curie(m[1], options) : $.uri(safeCurie);
+  };
 
    /**
     * Creates a CURIE string from a URI string.
@@ -131,8 +131,8 @@
     * @param {string} [options.defaultNamespace]  If the input URI starts with this value, the generated CURIE will have no namespace prefix and will not start with a colon.
     * @param {Object} [options.namespaces] A map of namespace bindings used to map CURIE prefixes to URIs.
     * @param {string[]} [options.reserved=['alternate', 'appendix', 'bookmark', 'cite', 'chapter', 'contents', 'copyright',
-			'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
-			'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
+      'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
+      'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up']]
                         A list of local names that will always be mapped to the URI specified by reservedNamespace.
     * @param {string} [options.charcase='preserve'] Specifies the case of the returned URI. Acceptable values are:
     * <dl>
@@ -143,32 +143,31 @@
     * If any other value is provided or the option is not specified, a default value of 'preserve' will be assumed.
     * @return A new {@link jQuery.uri} object representing the full absolute URI specified by the CURIE.
     */
-	$.createCurie = function (uri, options) {
-		var opts = $.extend({}, $.curie.defaults, options || {}),
-		  ns = opts.namespaces,
-		  curie;
-		uri = $.uri(uri).toString();
-	  if (uri.substring(0, opts.reservedNamespace.toString().length) === opts.reservedNamespace.toString()) {
-	    curie = uri.substring(opts.reservedNamespace.toString().length);
-	    if ($.inArray(curie, opts.reserved) === -1) {
-	      curie = ':' + curie;
-	    }
-	  }
-	  if (curie === undefined) {
-		$.each(ns, function (prefix, namespace) {
-		  if (uri.substring(0, namespace.toString().length) === namespace.toString()) {
-		    curie = prefix + ':' + uri.substring(namespace.toString().length);
-		    return null;
-		  }
-		});
-
-	  }
-		if (curie === undefined) {
-  		throw "No Namespace Binding: There's no appropriate namespace binding for generating a CURIE from " + uri;
-		} else {
-		  return curie;
-		}
-	};
+  $.createCurie = function (uri, options) {
+    var opts = $.extend({}, $.curie.defaults, options || {}),
+      ns = opts.namespaces,
+      curie;
+    uri = $.uri(uri).toString();
+    if (opts.reservedNamespace !== undefined && 
+        uri.substring(0, opts.reservedNamespace.toString().length) === opts.reservedNamespace.toString()) {
+      curie = uri.substring(opts.reservedNamespace.toString().length);
+      if ($.inArray(curie, opts.reserved) === -1) {
+        curie = ':' + curie;
+      }
+    } else {
+      $.each(ns, function (prefix, namespace) {
+        if (uri.substring(0, namespace.toString().length) === namespace.toString()) {
+          curie = prefix + ':' + uri.substring(namespace.toString().length);
+          return null;
+        }
+      });
+    }
+    if (curie === undefined) {
+      throw "No Namespace Binding: There's no appropriate namespace binding for generating a CURIE from " + uri;
+    } else {
+      return curie;
+    }
+  };
 
    /**
     * Creates a {@link jQuery.uri} object by parsing the specified
@@ -184,10 +183,10 @@
     *        be included in the options.namespaces property.
     * @see jQuery.curie
     */
-	$.fn.curie = function (curie, options) {
-		var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
-		return $.curie(curie, opts);
-	};
+  $.fn.curie = function (curie, options) {
+    var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
+    return $.curie(curie, opts);
+  };
 
    /**
     * Creates a {@link jQuery.uri} object by parsing the specified
@@ -204,10 +203,10 @@
     *        be included in the options.namespaces property.
     * @see jQuery.safeCurie
     */
-	$.fn.safeCurie = function (safeCurie, options) {
-		var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
-		return $.safeCurie(safeCurie, opts);
-	};
+  $.fn.safeCurie = function (safeCurie, options) {
+    var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
+    return $.safeCurie(safeCurie, opts);
+  };
 
    /**
     * Creates a CURIE string from a URI string using the namespace
@@ -223,20 +222,20 @@
     *        be included in the options.namespaces property.
     * @see jQuery.createCurie
     */
-	$.fn.createCurie = function (uri, options) {
-		var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
-		return $.createCurie(uri, opts);
-	};
+  $.fn.createCurie = function (uri, options) {
+    var opts = $.extend({}, $.fn.curie.defaults, { namespaces: this.xmlns() }, options || {});
+    return $.createCurie(uri, opts);
+  };
 
-	$.fn.curie.defaults = {
-		reserved: [
-			'alternate', 'appendix', 'bookmark', 'cite', 'chapter', 'contents', 'copyright',
-			'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
-			'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up'
-		],
-		reservedNamespace: 'http://www.w3.org/1999/xhtml/vocab#',
-		defaultNamespace: undefined,
-		charcase: 'lower'
-	};
+  $.fn.curie.defaults = {
+    reserved: [
+      'alternate', 'appendix', 'bookmark', 'cite', 'chapter', 'contents', 'copyright',
+      'first', 'glossary', 'help', 'icon', 'index', 'last', 'license', 'meta', 'next',
+      'p3pv1', 'prev', 'role', 'section', 'stylesheet', 'subsection', 'start', 'top', 'up'
+    ],
+    reservedNamespace: 'http://www.w3.org/1999/xhtml/vocab#',
+    defaultNamespace: undefined,
+    charcase: 'lower'
+  };
 
 })(jQuery);
